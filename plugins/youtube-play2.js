9932a584eba36b8ba7996402cde91781f3e41036
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 09:45:16 +0700

    p

--- plugins/youtube-play2.js
+++ plugins/youtube-play2.js
@@ -7,9 +7,9 @@ if (command == 'play.1') {
 conn.reply(m.chat, `*_⏳Wait,Audio Sedang Di Proses..._⏳*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'AUDIO',
-body: 'Sad-Bot',         
-previewType: 0, thumbnail: fs.readFileSync("./thumbnail.jpg"),
-sourceUrl: `https://github.com/FahriAdison/Sad-Multi-Device`}}})
+body: 'YukiBot',         
+previewType: 0, thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+sourceUrl: `https://youtu.be/pwLZpdfO8AU`}}})
 let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
 let json = await res.json()
 conn.sendFile(m.chat, json.result.url, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
@@ -17,12 +17,12 @@ if (command == 'play.2') {
 conn.reply(m.chat, `*_⏳ Wait,Video Sedang Di Proses...⏳_*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'VIDEO',
-body: 'Fangz BOT',         
-previewType: 0, thumbnail: fs.readFileSync("./thumbnail.jpg"),
-sourceUrl: `https://github.com/Fangz2007/Hello-MD-multi-device`}}})
+body: 'ᴹᴿ᭄ King Of Bear ×፝֟͜×',         
+previewType: 0, thumbnail: await (await fetch('https://telegra.ph/file/6ab4daac226292a112540.jpg')).buffer(),
+sourceUrl: `https://youtu.be/pwLZpdfO8AU`}}})
 let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp4?text="+text)
 let json = await res.json()
-conn.sendFile(m.chat, json.result.url, 'error.mp4', `_Fangz BOT_`, m)}
+conn.sendFile(m.chat, json.result.url, 'error.mp4', `_ᴹᴿ᭄ King Of Bear ×፝֟͜×_`, m)}
 }catch(e){
 m.reply('*[❗INFO❗] ERROR,TIDAK DAPAT MENCARI LAGU TERSEBUT*')
 console.log(e)

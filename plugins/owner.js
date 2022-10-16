28af5156a3c8e267b051c490e8fd655b87347d1e
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 15:09:51 +0700

    p

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,13 +1,21 @@
+import fetch from 'node-fetch'
 import fs from 'fs';
 import crypto from 'crypto'
 let handler = async (m, { conn, text, usedPrefix }) => {
-
+    const ultah = new Date('November 4 2022 23:59:59')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
     let sewa = `
          〔 llı OWNER ${namebot} ıll 〕
     
 Nama: ${nameowner}
 Umur: ${umurowner}
 Asal: ${asalowner}
+Status: ${pacarowner}
 
 Nomor: ${nomorowner}
 
@@ -15,16 +23,16 @@ ${botdate}
 `
         let img1 = fs.readFileSync('./thumbnail.jpg');
         let img2 = fs.readFileSync('./media/bawah.png');
-    return conn.sendButton(m.chat, hiasan, sewa, intro, [["Menu", usedPrefix + "menu"], ["Sewa Bot", usedPrefix + "sewa"]], m, {
+    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://telegra.ph/file/6ab4daac226292a112540.jpg`)).buffer(), [["Menu", usedPrefix + "menu"], ["Sewa Bot", usedPrefix + "sewa"]], m, {
         contextInfo: {
             externalAdReply: {
                 mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: bottime,
-                        body: '⚘ ᴄʀᴇᴀᴛᴇ ʙʏ ʀʏʜᴀʀ',          previewType: 0,
-                        thumbnail: img1,
-                        sourceUrl: sgh
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
             }
         }
     })

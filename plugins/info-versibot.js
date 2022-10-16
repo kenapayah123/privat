f6a1dde68aff30b23e82a3cdf73cc2a3b2076f12
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 19:33:33 +0700

    p

--- plugins/info-versibot.js
+++ plugins/info-versibot.js
@@ -1,7 +1,14 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `Bot ini menggunakan script by RyHar\n\n*Version:* Private Edition V3`
+  const ultah = new Date('November 4 2022 00:00:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+  let info = `Bot ini menggunakan script by\n ᴹᴿ᭄ King Of Bear ×፝֟͜×\n\n*Version:* Private Edition V3`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 const message = {
@@ -13,20 +20,31 @@ const message = {
                 {
                     urlButton: {
                         displayText: 'Beli Sc? Klik',
-                        url: 'https://wa.me/6282182461800'
+                        url: 'https://wa.me/6288279268363'
                     }
                 },
                 {
                     urlButton: {
                         displayText: 'Ke YouTube',
-                        url: 'https://m.youtube.com/channel/UCK24H0dr65L7KqwENHhX6Bg',
+                        url: 'https://youtu.be/pwLZpdfO8AU',
                     }
                 },
             ]
         }
         conn.sendMessage(m.chat, message)
-conn.reply(m.chat, info, m, { contextInfo: { externalAdReply: {title: global.wm, body: 'Script By FangzXD', sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
-}
+conn.reply(m.chat, info, m, {
+contextInfo: { externalAdReply :{ showAdAttribution: true,
+                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU',
+                        mediaType: 2,
+                        description: `⚘ ᴄʀᴇᴀᴛᴇ ᴹᴿ᭄ King Of Bear ×፝֟͜×`,
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `⚘ ᴄʀᴇᴀᴛᴇ by ᴹᴿ᭄ King Of Bear ×፝֟͜×`,          previewType: 0,
+                        thumbnail: await (await fetch(giflogo2)).buffer(),
+                        mediaUrl: 'https://youtu.be/pwLZpdfO8AU'
+                        
+                      }}
+})
+  }
 handler.help = ['cekversi']
 handler.tags = ['info']
 handler.command =  /^(cekversi)$/i

d180ed71fba21bab43eabab74c2ab52263a83e59
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 15:51:49 +0700

    p

--- plugins/owner-boton.js
+++ plugins/owner-boton.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Sukses Menghidupkan FANGZ BOT 🤖* ')
+    m.reply(' *Sukses Menghidupkan Yuki Adistyラ 🤖* ')
 }
 
 

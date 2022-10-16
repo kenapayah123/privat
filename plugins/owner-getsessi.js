d0dda97fb47c02462f9a54aa8ba10d3bba2c7a47
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 15:38:03 +0700

    p

--- plugins/owner-getsessi.js
+++ plugins/owner-getsessi.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 let handler = async (m, { conn, text }) => {
     m.reply('Tunggu Sebentar, Sedang mengambil file sesi mu')
-    let sesi = await fs.readFileSync('./session.data.json')
-    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'session.data.json' }, { quoted: m })
+    let sesi = await fs.readFileSync('./KingOfBear.data.json')
+    return await conn.sendMessage(m.chat, { document: sesi, mimetype: 'application/json', fileName: 'KingOfBear.data.json' }, { quoted: m })
 }
 handler.help = ['getsessi']
 handler.tags = ['owner']

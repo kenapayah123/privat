3dedb5ad2726436898f30c9855b37cef34aefd66
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 15:28:01 +0700

    p

--- plugins/owner-tesbot.js
+++ plugins/owner-tesbot.js
@@ -1,21 +1,35 @@
-import fs from 'fs'
-let handler = async (m, { conn, args, command }) => {
-let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length;
- await conn.sendButton(m.chat, `*🎏 Aktif Kak*\n`,wm + '\n\n' + botdate, [['🗳️MENU','.menu'],['🔭DONASI','.donasi']], m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
+import fetch from 'node-fetch'
+import fs from 'fs';
+import crypto from 'crypto'
+let handler = async (m, { conn, text, usedPrefix }) => {
+    const ultah = new Date('November 4 2022 23:59:59')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+    let sewa = `*🎏 Bot Aktif Kak*
+
+${wm}
+${botdate}
+`
+        let img1 = fs.readFileSync('./thumbnail.jpg');
+        let img2 = fs.readFileSync('./media/bawah.png');
+    return conn.sendButton(m.chat, hiasan, sewa, await (await fetch(`https://telegra.ph/file/6ab4daac226292a112540.jpg`)).buffer(), [["🗳️MENU", usedPrefix + "menu"], ["🔭DONASI", usedPrefix + "donasi"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: bottime,
-                        body: 'BOT AKTIF',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: sgc
-                      }}
-})
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+            }
+        }
+    })
 }
-
 handler.help = ['tesbot']
 handler.tags = ['owner']
 handler.command = ['tesbot']

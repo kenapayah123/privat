2b43b053b509dd380709a7b59e47e42ff73ecc2e
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-16 19:59:14 +0700

    p

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -1,5 +1,24 @@
+import fetch from 'node-fetch'
 import fs from 'fs'
+import moment from 'moment-timezone'
 let handler = async (m, { conn, args, command }) => {
+  let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let who = m.sender
+const time = moment.tz('Asia/Jakarta').format('HH')
+  const ultah = new Date('November 4 2022 00:00:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+  let name = await conn.getName(m.sender)
+  let runnya = `┏━━━ꕥ〔 *RUNTIME BOT* 〕ꕥ━⬣
+┃✾ ⏲️ Waktu: ${time} WIB
+┃✾ 💌 Aktif Selama : ${uptime}
+┃✾ 🌹 Tag : ${name}
+┗━━━━━━ꕥ`
 	let _muptime
     if (process.send) {
       process.send('uptime')
@@ -9,17 +28,19 @@ let handler = async (m, { conn, args, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
- await conn.sendButton(m.chat, `${muptime}\n`,wm + '\n\n' + botdate, thumb, [['🥞MENU','.listmenu']], m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
+ await conn.sendButton(m.chat, runnya,wm + '\n\n' + botdate, intro, [['🥞MENU','.listmenu']], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: bottime,
-                        body: '✦ UPTIME BOT',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
-                        sourceUrl: sgh
-                      }}
-})
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+            }
+        }
+    })
 }
 
 
@@ -29,9 +50,8 @@ handler.command = ['runtime', 'rt']
 export default handler
 
 function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Days ☀️*\n ', h, ' *Hours 🕐*\n ', m, ' *Minute ⏰*\n ', s, ' *Second ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }

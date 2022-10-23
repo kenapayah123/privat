4dee09b4adad044627838e7088b21280db88dae1
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-23 20:40:22 +0700

    p

--- plugins/internet-ssweb.js
+++ plugins/internet-ssweb.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
   import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command, args }) => {
-  
-  let res = await(await fetch(`https://saipulanuar.herokuapp.com/api/download/ssweb?url=${args[0]}&apikey=FreeApikey`)).buffer()
+  let bear = "danzz"
+  let res = await(await fetch(`https://saipulanuar.ga/api/download/ssweb?url=${args[0]}&apikey=${bear}`)).buffer()
   if(!args[0]) throw `Linknya mana?`
 conn.sendHydrated(m.chat, `*「 Screenshot Web 」*\n\n*💻 Url:* ${args[0]}`, wm, res, `${args[0]}`, '🌎 U R L', null, null, [[null,null],[null,null],[null,null]], m)
 }

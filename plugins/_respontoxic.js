67bd4a2bd90ca20a6182afda743721db367c6574
saipulanuar <77055141+saipulanuar@users.noreply.github.com>
2022-10-23 21:07:32 +0700

    Update

--- plugins/_respontoxic.js
+++ plugins/_respontoxic.js
@@ -1,20 +1,53 @@
 import fetch from 'node-fetch'
+import fs from 'fs'
+import moment from 'moment-timezone'
+let handler = async (m, { conn, args, command }) => {
+  let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let who = m.sender
+const time = moment.tz('Asia/Jakarta').format('HH')
+  const ultah = new Date('November 4 2022 00:00:01')
+    const sekarat = new Date().getTime() 
+    const Kurang = ultah - sekarat
+    const ohari = Math.floor( Kurang / (1000 * 60 * 60 * 24));
+    const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
+    const detek = Math.floor( Kurang % (1000 * 60) / 1000)
+  let name = await conn.getName(m.sender)
+  let runnya = `━━━ꕥ〔 *SOURCE CODE* 〕ꕥ━⬣
+✾ *Script Private | Mau Buy? Silahkan Hubungi Owner!*
 
-let handler  = async (m, { conn, usedPrefix }) => {
-let info = `
-
-*--- SOURCE CODE ---*
-* | Script Private | Mau Buy? Silahkan Hubungi Owner!*
-
-*--- OFFICIAL GROUP ---*
+✾ *OFFICIAL GROUP*
 _https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL_
 
-*--- YOUTUBE OFFICIAL ---*
+✾ *YOUTUBE OFFICIAL*
 _https://youtu.be/pwLZpdfO8AU_
-`.trim()
-
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Script By ᴹᴿ᭄ King Of Bear ×፝֟͜× | Buy? 50K No Enc*', 'status@broadcast')
+━━━━━━ꕥ`
+  let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+ await conn.sendButton(m.chat, runnya,wm + '\n\n' + botdate, intro, [['🧑OWNER','.owner']], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: '',
+                        mediaType: 2,
+                        description: 'anu',
+                        title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        body: `Subscribe YT My Bestie`,                                       previewType: 0,
+                        thumbnail: await (await fetch(`https://i.ibb.co/jfZVKmC/babi2.jpg`)).buffer(),
+                        sourceUrl: 'https://youtu.be/pwLZpdfO8AU'
+            }
+        }
+    })
 }
+
+
 handler.help = ['sc', 'sewasc']
 handler.tags = ['info', 'main']
 
@@ -32,4 +65,11 @@ handler.botAdmin = false
 handler.fail = null
 
 export default handler 
-//*
+
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
